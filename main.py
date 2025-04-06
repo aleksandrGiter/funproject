6cec899e924ff053adb85e8df4fb34d665973c2c
aleksandrGiter <cat.black.193.cat@gmail.com>
2025-04-06 11:30:52 +0700

    first

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("ll")
+import tkinter
+
+window = tkinter.Tk()
+text = tkinter.Label(text="this button does nothing", anchor='center')
+text.place(relx=0.5, rely=0.1)
+button = tkinter.Button(text="press me!", anchor='center')
+button.place(relx=0.5, rely=0.5)
+
+tkinter.mainloop()
